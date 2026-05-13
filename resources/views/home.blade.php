6369e77b3ae244292cf4a0552f386625c154fca2
Andrei Gurdumov <andreigurd@gmail.com>
2026-05-13 00:14:14 -0400

    Updated home blade with chirps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,18 +28,19 @@
         Welcome
     </x-slot:title>
 
-    <div class="max-w-2xl mx-auto">
-        <div class="card bg-base-100 shadow mt-8">
-            <div class="card-body">
-                <div>
-                    <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                    <p>This is your brand new Laravel app. Time to make it sing (or chirp)!</p>
-                    <p class="mt-2 text-sm text-gray-600">Now this is live on the internet! 🎉</p>
+        <div class="max-w-2xl mx-auto">
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                            <div class="font-semibold">{{ $chirp['author'] }}</div>
+                            <div class="mt-1">{{ $chirp['message'] }}</div>
+                            <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
-    </div>
-</x-layout>
+    </x-layout>
 
     <footer class="footer footer-center p-5 bg-base-300 text-base-content text-xs">
         <div>
