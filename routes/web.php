b7cc8ff4209f52161c057edb04b3636f23dcc152
Andrei Gurdumov <andreigurd@gmail.com>
2026-05-18 16:39:02 -0400

    Implemented protected routes functionality.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,27 @@
 <?php
 
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\Register;
+
+// Registration routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
 
 Route::get('/', [ChirpController::class, 'index']);
 
 Route::resource('chirps', ChirpController::class)
     ->only(['store', 'edit', 'update', 'destroy']);
+
+Route::get('/', [ChirpController::class, 'index']);
+
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::post('/chirps', [ChirpController::class, 'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+});
